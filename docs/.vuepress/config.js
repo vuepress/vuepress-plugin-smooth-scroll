2e757b229c901a470e569e6aedf238d291cfc067
Xinyu Liu <meteor.lxy@foxmail.com>
2019-02-25 19:06:36 +0800

    chore: refine docs config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,31 @@
 module.exports = {
   base: '/vuepress-plugin-smooth-scroll/',
 
-  title: 'vuepress-plugin-smooth-scroll',
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'vuepress-plugin-smooth-scroll',
+      description: 'Vuepress plugin for smooth scrolling',
+    },
+  },
+
+  themeConfig: {
+    repo: 'meteorlxy/vuepress-plugin-smooth-scroll',
+
+    editLinks: true,
+
+    docsDir: 'docs',
+
+    search: false,
+
+    locales: {
+      '/': {
+        label: 'English',
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated',
+      },
+    },
+  },
 
   plugins: [
     require.resolve('../../lib'),
